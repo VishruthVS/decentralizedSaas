824184377e8f97933b99458b9c9c80f917e5c50c
VishruthVS <vishruthvs2003@gmail.com>
2024-06-02 22:58:48 +0530

    initial backend

---
+++ backend/src/routers/user.ts
@@ -0,0 +1,172 @@
+import { S3Client } from "@aws-sdk/client-s3";
+import { createPresignedPost } from "@aws-sdk/s3-presigned-post";
+import { PrismaClient } from "@prisma/client";
+import { Router } from "express";
+import jwt from "jsonwebtoken";
+import { JWT_SECRET } from "../config";
+import { authMiddleware } from "../middleware";
+import { createTaskInput } from "../types";
+const DEFAULT_TITLE = "Select the most clickable thumbnail";
+const s3Client = new S3Client({
+    credentials: {
+        accessKeyId: process.env.ACCESS_KEY_ID ?? "",
+        secretAccessKey: process.env.ACCESS_SECRET ?? "",
+    },
+    region: "us-east-1"
+});
+const router = Router();
+const prismaClient = new PrismaClient();
+
+
+router.get("/task", authMiddleware, async (req, res) => {
+    // @ts-ignore
+    const taskId: string = req.query.taskId;
+    // @ts-ignore
+    const userId: string = req.userId;
+
+    const taskDetails = await prismaClient.task.findFirst({
+        where: {
+            user_id: Number(userId),
+            id: Number(taskId)
+        },
+        include: {
+            options: true
+        }
+    })
+
+    if (!taskDetails) {
+        return res.status(411).json({
+            message: "You dont have access to this task"
+        })
+    }
+
+    // Todo: Can u make this faster?
+    const responses = await prismaClient.submission.findMany({
+        where: {
+            task_id: Number(taskId)
+        },
+        include: {
+            option: true
+        }
+    });
+
+    const result: Record<string, {
+        count: number;
+        option: {
+            imageUrl: string
+        }
+    }> = {};
+
+    taskDetails.options.forEach(option => {
+        result[option.id] = {
+            count: 0,
+            option: {
+                imageUrl: option.image_url
+            }
+        }
+    })
+
+    responses.forEach(r => {
+        result[r.option_id].count++;
+    });
+
+    res.json({
+        result,
+        taskDetails
+    })
+
+})
+
+router.post("/task", authMiddleware, async (req, res) => {
+    //@ts-ignore
+    const userId = req.userId;
+
+    const body = req.body;
+
+    const parsedData = createTaskInput.safeParse(body);
+
+    if (!parsedData.success) {
+        res.status(411).json({
+            message: "You have sent the wrong inputs"
+        })
+    }
+    let response = await prismaClient.$transaction(async tx => {
+
+        const response = await tx.task.create({
+            data: {
+                //@ts-ignore
+                title: parsedData.data.title ?? DEFAULT_TITLE,
+                amount: 0.1,
+                //TODO: Signature should be unique in the table else people can reuse a signature
+                //@ts-ignore
+                signature: parsedData.data.signature,
+                user_id: userId
+            }
+        });
+
+        await tx.option.createMany({
+            //@ts-ignore
+            data: parsedData.data.options.map(x => ({
+                image_url: x.imageUrl,
+                task_id: response.id
+            }))
+        })
+
+        return response;
+
+    })
+    res.json({
+        id: response.id
+    })
+})
+router.get("/presignedUrl", authMiddleware, async (req, res) => {
+    // @ts-ignore
+    const userId = req.userId;
+
+    const { url, fields } = await createPresignedPost(s3Client, {
+        Bucket: 'decentralized-saas',
+        Key: `saas/${userId}/${Math.random()}/image.jpg`,
+        Conditions: [
+            ['content-length-range', 0, 5 * 1024 * 1024] // 5 MB max
+        ],
+        Expires: 3600
+    })
+
+    res.json({
+        preSignedUrl: url,
+        fields
+    })
+
+})
+
+router.post("/signin", async (req, res) => {
+    const hardCodedWalletAddress = "0xCDAF44CE32B7f1CdA63d1d2D2b8F47951377A670"
+    const existingUser = await prismaClient.user.findFirst({
+        where: {
+            address: hardCodedWalletAddress
+        }
+    })
+    if (existingUser) {
+        const token = jwt.sign({
+            userId: existingUser.id
+        }, JWT_SECRET)
+        res.json({
+            token
+        })
+    } else {
+        const user = await prismaClient.user.create({
+            data: {
+                address: hardCodedWalletAddress
+            }
+        })
+
+        const token = jwt.sign({
+            userId: user.id
+        }, JWT_SECRET)
+        res.json({
+            token
+        })
+    }
+});
+
+export default router;
---
+++ backend/src/routers/worker.ts
@@ -0,0 +1,209 @@
+import { PrismaClient } from "@prisma/client";
+import { Router } from "express";
+import jwt from "jsonwebtoken";
+import { WORKER_JWTSECRET } from "../config";
+import { getNextTask } from "../db";
+import { workerMiddleware } from "../middleware";
+import { createSubmissionInput } from "../types";
+const router = Router();
+const prismaClient = new PrismaClient();
+const TOTAL_SUBMISSIONS = 100;
+router.post("/signin", async (req, res) => {
+    const hardCodedWalletAddress = "0xCDAF44CE32B7f1CdA63d1d2D2b8F47951377A670"
+    const existingUser = await prismaClient.worker.findFirst({
+        where: {
+            address: hardCodedWalletAddress
+        }
+    })
+    if (existingUser) {
+        const token = jwt.sign({
+            userId: existingUser.id
+        }, WORKER_JWTSECRET)
+        res.json({
+            token
+        })
+    } else {
+        const user = await prismaClient.worker.create({
+            data: {
+                address: hardCodedWalletAddress,
+                pending_amount: 0,
+                locked_amount: 0
+            }
+        })
+
+        const token = jwt.sign({
+            userId: user.id
+        }, WORKER_JWTSECRET)
+        res.json({
+            token
+        })
+    }
+});
+router.get("/balance", workerMiddleware, async (req, res) => {
+    // @ts-ignore
+    const userId: string = req.userId;
+
+    const worker = await prismaClient.worker.findFirst({
+        where: {
+            id: Number(userId)
+        }
+    })
+
+    res.json({
+        pendingAmount: worker?.pending_amount,
+        lockedAmount: worker?.pending_amount,
+    })
+})
+router.post("/payout", workerMiddleware, async (req, res) => {
+    // @ts-ignore
+    const userId: string = req.userId;
+    const worker = await prismaClient.worker.findFirst({
+        where: { id: Number(userId) }
+    })
+
+    if (!worker) {
+        return res.status(403).json({
+            message: "User not found"
+        })
+    }
+    /*
+        const transaction = new Transaction().add(
+            SystemProgram.transfer({
+                fromPubkey: new PublicKey("2KeovpYvrgpziaDsq8nbNMP4mc48VNBVXb5arbqrg9Cq"),
+                toPubkey: new PublicKey(worker.address),
+                lamports: 1000_000_000 * worker.pending_amount / TOTAL_DECIMALS,
+            })
+        );
+    
+    
+        console.log(worker.address);
+    
+        const keypair = Keypair.fromSecretKey(decode(privateKey));
+    
+        // TODO: There's a double spending problem here
+        // The user can request the withdrawal multiple times
+        // Can u figure out a way to fix it?
+        let signature = "";
+        try {
+            signature = await sendAndConfirmTransaction(
+                connection,
+                transaction,
+                [keypair],
+            );
+    
+        } catch (e) {
+            return res.json({
+                message: "Transaction failed"
+            })
+        }
+    
+        console.log(signature)
+    */
+    // We should add a lock here
+    console.log(worker.address);
+    const address = worker.address;
+    const tnxId = "0x123123123"
+    await prismaClient.$transaction(async tx => {
+        await tx.worker.update({
+            where: {
+                id: Number(userId)
+            },
+            data: {
+                pending_amount: {
+                    decrement: worker.pending_amount
+                },
+                locked_amount: {
+                    increment: worker.pending_amount
+                }
+            }
+        })
+
+        await tx.payouts.create({
+            data: {
+                user_id: Number(userId),
+                amount: worker.pending_amount,
+                status: "Processing",
+                signature: tnxId
+            }
+        })
+    })
+
+    res.json({
+        message: "Processing payout",
+        amount: worker.pending_amount
+    })
+
+
+})
+router.post("/submission", workerMiddleware, async (req, res) => {
+    // @ts-ignore
+    const userId = req.userId;
+    const body = req.body;
+    const parsedBody = createSubmissionInput.safeParse(body);
+
+    if (parsedBody.success) {
+        const task = await getNextTask(Number(userId));
+        if (!task || task?.id !== Number(parsedBody.data.taskId)) {
+            return res.status(411).json({
+                message: "Incorrect task id"
+            })
+        }
+
+        const amount = (Number(task.amount) / TOTAL_SUBMISSIONS).toString();
+
+        const submission = await prismaClient.$transaction(async tx => {
+            const submission = await tx.submission.create({
+                data: {
+                    option_id: Number(parsedBody.data.selection),
+                    worker_id: userId,
+                    task_id: Number(parsedBody.data.taskId),
+                    amount: Number(amount)
+                }
+            })
+
+            await tx.worker.update({
+                where: {
+                    id: userId,
+                },
+                data: {
+                    pending_amount: {
+                        increment: Number(amount)
+                    }
+                }
+            })
+
+            return submission;
+        })
+
+        const nextTask = await getNextTask(Number(userId));
+        res.json({
+            nextTask,
+            amount
+        })
+
+
+    } else {
+        res.status(411).json({
+            message: "Incorrect inputs"
+        })
+
+    }
+
+})
+router.get("/nextTask", workerMiddleware, async (req, res) => {
+    // @ts-ignore
+    const userId: string = req.userId;
+
+    const task = await getNextTask(Number(userId));
+
+    if (!task) {
+        res.status(411).json({
+            message: "No more tasks left for you to review"
+        })
+    } else {
+        res.json({
+            task
+        })
+    }
+})
+export default router;
